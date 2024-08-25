3cc328fac3d60ca119484f4011b13f8154a00a13
Saqibkhan15 <aqib12u@gmail.com>
2024-08-25 11:58:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// Assignment No 1 Alerts
+// Q1
+// alert("hello world")
+// Q2
+// alert("Error! please enter a valid password")
+// Q3
+// alert("Welcome to JS land \nHappy Coding ")
+// Q4
+// alert(" Welcome to JS land...")
+// Q5
+// alert("Hello i can tun JS through my web browser's console ")
+
+// Assignment No 2 Variables
+
+
+// Q1
+// var username = "jhon doe"
+// alert(username)
+// Q2
+// var age = "15 years old"
+// alert(age)
+// Q3
+// var job = "Certified Mobile Application Devolpment"
+// alert(job)
+// Q4
+// var food = "pizza \n pizz \n piz \n pi \n p"
+// alert(food)
+// Q5
+// var email = "Saqib12u@gmail.com"
+// alert(email)
+// Q6
+// var biodata = "Saqib age 29 "
+// alert(biodata)
+
+// Q7
+
+// var book = "A smarter way to learn JavaScript"
+// alert(book)
+
+// Q8
+// document.write("Read a little, practice a lot. It's the system that's been proven by 200,000 learners—and praised in 4,000 5-star Amazon customer reviews.");
+
+// // Q9
+// var style = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(style)
